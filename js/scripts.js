fe29c4ee2befd4e01a7bf0f6c14bf3b8a478fc70
Rwll <rowell.dcanlapan@gmail.com>
2014-04-11 19:54:47 +0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	function scrollToAnchor(aid){
+	    var aTag = $("a[name='"+ aid +"']");
+	    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+	}
+
+	$('#button1').click(function(){
+		scrollToAnchor("first");
+	});
+
+	$('#button2').click(function(){
+		scrollToAnchor("second");
+	});
+
+	$('#button3').click(function(){
+		scrollToAnchor("third");
+	});
+
+	$('#button4').click(function(){
+		scrollToAnchor("top");
+	});
+	  
+});
